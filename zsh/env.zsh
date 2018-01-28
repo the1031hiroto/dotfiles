1a6d78a698f9287b9d68bd39716dca5177fba828
Shuji Koike <shuji.koike@me.com>
2018-01-29 07:34:51 +0900

    update zsh/env.zsh

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,24 +1,11 @@
 # rbenv
 if [ -d ${HOME}/.rbenv ]; then
-  export PATH="${HOME}/.rbenv/bin:${HOME}/.rbenv/shims:${PATH}"
   eval "$(rbenv init -)"
 fi
-
-# Android
-if [ -d "${HOME}/Applications/android-platform-tools" ]; then
-  export PATH="${HOME}/Applications/android-platform-tools:${PATH}"
-fi
-
-#THIS MUST BE AT THE END OF THE FILE FOR GVM TO WORK!!!
-[[ -s "${HOME}/.gvm/bin/gvm-init.sh" ]] && source "${HOME}/.gvm/bin/gvm-init.sh"
-
-# python env
-[[ -s "${HOME}/.pythonbrew/etc/bashrc" ]] && source "${HOME}/.pythonbrew/etc/bashrc"
 
 # go
 if [ -d "/usr/local/opt/go" ]; then
   export GOPATH="/usr/local/opt/go"
-  export PATH="/usr/local/opt/go/bin:$PATH"
 fi
 
 # original commands
