a77366b22c571b7a65c5644ec44fc90777834e77
Shuji Koike <shuji.koike@me.com>
2018-02-04 06:22:01 +0900

    alias dtail='docker logs --tail 10 -f

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -40,8 +40,10 @@ alias stop='brew services stop'
 alias restart='brew services restart'
 alias upgrade='brew update && brew upgrade && brew cleanup && brew leaves'
 
+alias di='docker images'
 alias images='docker images'
 alias images-pull='docker images | tail -n +2 | awk '{print $1 ":" $2}' | grep -v ":<none>" | xargs -n1 docker pull'
 alias images-clean='docker system prune -f'
+alias dtail='docker logs --tail 10 -f'
 
 alias ppp='echo -n `pwd` | pbcopy'
