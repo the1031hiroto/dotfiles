459d36574fe3e39090deb48152319b06d330e9f4
Shuji Koike <shuji.koike@me.com>
2018-02-04 22:08:39 +0900

    fix images-pull

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -42,7 +42,7 @@ alias upgrade='brew update && brew upgrade && brew cleanup && brew leaves'
 
 alias di='docker images'
 alias images='docker images'
-alias images-pull='docker images | tail -n +2 | awk '{print $1 ":" $2}' | grep -v ":<none>" | xargs -n1 docker pull'
+alias images-pull='docker images | tail -n +2 | awk '\''{print $1 ":" $2}'\'' | grep -v ":<none>" | xargs -n1 docker pull'
 alias images-clean='docker system prune -f'
 alias dtail='docker logs --tail 10 -f'
 
