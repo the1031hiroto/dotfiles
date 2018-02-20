eb3b1d1bf5b9120df9837de35a2e1f75c44de44f
Shuji Koike <shuji.koike@me.com>
2018-02-21 05:43:45 +0900

    up down

--- zsh/docker.zsh
+++ zsh/docker.zsh
@@ -1,1 +1,3 @@
 alias pma='open http://localhost:8033/; docker run -it --rm -e PMA_HOST=docker.for.mac.localhost -p 8033:80 phpmyadmin/phpmyadmin'
+alias up='docker-compose up -d'
+alias down='docker-compose down'
