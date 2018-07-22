5d900bc5318478e972dba906e246035ccef9510f
Shuji Koike <shuji.koike@me.com>
2018-07-23 08:29:52 +0900

    --name pma

--- zsh/docker.zsh
+++ zsh/docker.zsh
@@ -1,4 +1,4 @@
-alias pma='open http://localhost:8033/; docker run -it --rm -e PMA_HOST=docker.for.mac.host.internal -p 8033:80 phpmyadmin/phpmyadmin'
+alias pma='open http://localhost:8033/; docker run -it --rm -e PMA_HOST=docker.for.mac.host.internal -p 8033:80 --name pma phpmyadmin/phpmyadmin'
 alias c9='docker run -it --rm -p 8009:80 -v $(pwd):/workspace kdelfour/cloud9-docker'
 alias up='docker-compose up --detach --remove-orphans'
 alias down='docker-compose down'
