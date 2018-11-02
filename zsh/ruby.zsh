d3b35623e5917b46bc86db4c0ee5fb2630d558e8
Shuji Koike <shuji.koike@me.com>
2018-11-02 21:09:16 +0900

    ruby.zsh

--- zsh/ruby.zsh
+++ zsh/ruby.zsh
@@ -1,2 +1,4 @@
 alias rspec='bundle exec spring rspec'
 alias rails='bundle exec spring rails'
+alias guard='bundle exec guard -G ../Guardfile'
+alias rspecx='npx nodemon -x bundle exec spring rspec'
